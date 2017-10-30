bb8e08abe5b1174124f06ddb534a22ad90e384fe
antonkashuba <kashuba.ant@gmail.com>
2017-10-30 12:51:07 +0200

    some chenges

--- www/js/app.js
+++ www/js/app.js
@@ -14,13 +14,22 @@ angular.module('ToDo', ['ionic'])
 })
 
 .controller("firstCtrl", function($scope,$ionicModal){
-  $scope.tasks = [
-    {title: "first", done: true},
-    {title: "second", done: false},
-    {title: "third", done: false},
-    {title: "fourth", done: false}
-  ];
+
+  if(! angular.isUndefined(window.localStorage['tasks'])){
+    $scope.tasks = JSON.parse(window.localStorage['tasks']);
+    $scope.default = 1;
+  }else{
+    $scope.tasks = [
+      {title: "first", done: true},
+      {title: "second", done: false},
+      {title: "third", done: false},
+      {title: "fourth", done: false}
+    ];
+    $scope.default = 0;
+  }
+
   $ionicModal.fromTemplateUrl('views/task.html',function(modal){
+
     $scope.taskModal = modal;
   },{
       scope: $scope,
@@ -28,7 +37,7 @@ angular.module('ToDo', ['ionic'])
   });
 
   $scope.currentTaskid = -1;
-
+  
   $scope.addNewTask = function(){
     $scope.taskModal.show();
     $scope.activeTask = {
@@ -58,6 +67,27 @@ angular.module('ToDo', ['ionic'])
 
   $scope.deleteTask = function(id){
     $scope.tasks.splice(id,1);
+    saveItems();
   }
+
+  $scope.submitTask = function(task){
+    if($scope.currentTaskid == -1){
+      $scope.tasks.push({
+        title: task.title,
+        done: false
+      });
+    } else{
+      var id = $scope.currentTaskid;
+      $scope.tasks[id].title = task.title;
+      $scope.tasks[id].done = task.done;
+    }
+    $scope.taskModal.hide();
+    saveItems();
+  }
+
+  function saveItems(){
+    window.localStorage['tasks'] = angular.toJson($scope.tasks);
+  }
+
 
 });
